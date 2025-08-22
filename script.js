cf3818f16c60a6d82540357ecd130fde91cdb9d4
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-08-23 00:53:59 +0500

    handle Card Title func

--- script.js
+++ script.js
@@ -178,3 +178,17 @@ function showSlide(n) {
                 card.removeEventListener('mousemove', handleCardTilt);
             });
         });
+          function handleCardTilt(e) {
+            const card = e.currentTarget;
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            
+            const centerX = rect.width / 2;
+            const centerY = rect.height / 2;
+            
+            const rotateX = (y - centerY) / 10;
+            const rotateY = (centerX - x) / 10;
+            
+            card.style.transform = `translateY(-15px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        }
