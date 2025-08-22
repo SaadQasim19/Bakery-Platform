aa8ac33a96c2213b38c08d5789b6cb80d06de4a3
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-08-23 00:52:11 +0500

    smooth scrolling for nav links

--- script.js
+++ script.js
@@ -124,3 +124,17 @@ function showSlide(n) {
                 icon.classList.remove('fa-times');
             });
         });
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    const headerHeight = document.querySelector('.header').offsetHeight;
+                    const targetPosition = target.offsetTop - headerHeight;
+                    window.scrollTo({
+                        top: targetPosition,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
